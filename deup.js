d3abd7b57735b10b4e2641b16224263527db747b
Felipe Plets <felipe.plets@zensurance.com>
2023-07-18 10:18:14 -0400

    fix: version 0.0.1

---
+++ deup.js
@@ -0,0 +1,67 @@
+#!/usr/bin/env node
+
+import { program } from "commander"
+import fs from "fs-extra"
+import { sync } from "glob"
+import ora from "ora"
+import shell from "shelljs"
+
+import packageJSON from "./package.json" assert { type: "json" }
+
+const getPackageFiles = () => sync("**/package.json", {
+    ignore: ["**/node_modules/**/package.json", "package.json"],
+})
+
+const removeDupes = async (packageFiles, packageName) => {
+    let maxVersion = ""
+    for (const packageFile of packageFiles) {
+        const packageJson = await fs.readJson(packageFile)
+        if (packageJson?.dependencies?.[packageName]) {
+            const currVersion = packageJson.dependencies[packageName].replace(/^\^|~/, "")
+            maxVersion = currVersion > maxVersion ? currVersion : maxVersion
+            delete packageJson.dependencies[packageName]
+            await fs.writeJson(packageFile, packageJson, { spaces: 2 })
+        }
+    }
+    return maxVersion
+}
+
+const addToRootAndInstall = async (packageName, maxVersion) => {
+    const rootPackageJson = await fs.readJson("package.json")
+    rootPackageJson.dependencies[packageName] = maxVersion
+    rootPackageJson.scripts["bootstrap"] = rootPackageJson.scripts["bootstrap"].replace("--ci", "--no-ci")
+    await fs.writeJson("package.json", rootPackageJson, { spaces: 2 })
+
+    shell.exec("npm install")
+
+    rootPackageJson.scripts["bootstrap"] = rootPackageJson.scripts["bootstrap"].replace("--no-ci", "--ci")
+    await fs.writeJson("package.json", rootPackageJson, { spaces: 2 })
+}
+
+const dedupe = async (packageName) => {
+    try {
+        const spinner = ora("Searching package.json files...").start()
+
+        const packageFiles = getPackageFiles()
+        spinner.text = `${packageFiles.length} package.json files found`
+
+        const maxVersion = await removeDupes(packageFiles, packageName)
+
+        spinner.text = `Adding ${packageName}@${maxVersion} to package.json and running npm install...`
+        await addToRootAndInstall(packageName, maxVersion)
+
+        spinner.succeed(`Dependency "${packageName}" updated and installed.`)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+const main = async () => {
+    program
+        .version(packageJSON.version)
+        .arguments("<packageName>")
+        .action(dedupe)
+    program.parse(process.argv)
+}
+await main()
+
